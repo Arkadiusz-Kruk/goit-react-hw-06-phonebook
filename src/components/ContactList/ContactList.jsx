b6f19c33565911f210dee292dcd6f2b4fcc9ea09
Arkadiusz-Kruk <92742646+Arkadiusz-Kruk@users.noreply.github.com>
2022-10-15 21:21:20 +0200

    upd
    u

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,42 +1,49 @@
 import styles from './ContactList.module.css';
 import { Notification } from 'components/Notification/Notification';
 import React from 'react';
-import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteContact } from 'redux/actions/contactsActions';
 
-
-const ContactList = ({ contacts, removeContact }) => {
+const ContactList = () => {
   const { wrapper, text, button } = styles;
+
+  const contacts = useSelector(state => state.contacts);
+  const filter = useSelector(state => state.filter);
+
+  const filteredContacts = contacts.filter(c =>
+    c.name.toLowerCase().includes(filter)
+  );
+
+  const dispatch = useDispatch();
+
+  const deleteItemContact = id => {
+    return dispatch(deleteContact(id));
+  };
 
   return (
     <>
-        {contacts.length > 0 ? (
-          <ul className={wrapper}>
-            {contacts.map(contact => {
-              return (
-                <li className={text} key={contact.id}>
-                  <span>{`${contact.name}: ${contact.number}`}</span>
-                  <button
-                    type="button"
-                    className={button}
-                    onClick={() => removeContact(contact.id)}
-                  >
-                    Delete
-                  </button>
-                </li>
-              );
-            })}
-          </ul>
-        ) : (
-          <Notification message="You don't have this contact" />
-        )}
-      </>
+      {filteredContacts.length > 0 ? (
+        <ul className={wrapper}>
+          {filteredContacts.map(contact => {
+            return (
+              <li className={text} key={contact.id}>
+                <span>{`${contact.name}: ${contact.number}`}</span>
+                <button
+                  type="button"
+                  className={button}
+                  onClick={() => deleteItemContact(contact.id)}
+                >
+                  Delete
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <Notification message="You don't have this contact" />
+      )}
+    </>
   );
 };
 
-
 export default ContactList;
-
-ContactList.propTypes = {
-  contacts: PropTypes.array.isRequired,
-  removeContact: PropTypes.func,
-};
